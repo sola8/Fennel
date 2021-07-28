1510e14fd663ccf3571d7af2087bb6e01d2ede32
sola8 <makeresola8@gmail.com>
2021-07-27 23:38:37 -0400

    Resolves #1

--- cogs/test.py
+++ cogs/test.py
@@ -1,9 +1,10 @@
 import discord
+import main
+import config
 
-from bot import export
 from discord.ext import commands
-from helpers.utils import *
-from config import *
+from helpers.constants import TEAM_MAP, STATS_TO_AVERAGE
+
 
 class Test(commands.Cog):
     def __init__(self, bot):
@@ -14,20 +15,53 @@ class Test(commands.Cog):
         await ctx.send(", ".join(args))
 
     # Search for player using pid or name
+    # Make a command group for this?
+    # Player bios, awards, ratings, stats (basic/advanced)
+    # Progressions and splits?
+    # Use hex info in export to determine embed color for each player
     @commands.command(aliases=('p',))
     async def player(self, ctx, arg):
+
+        season = main.get_season()
+
+        def find_player_name(player):
+            if len(player['lastName']) == 0:
+                return player['firstName'].strip()
+            if len(player['firstName']) == 0:
+                return player['lastName'].strip()
+            return player['firstName'].strip() + " " + player['lastName'].strip()
 
         arg = arg.strip()
 
         if arg.isdigit():
-            pid = int(arg)
-    
-        pid = arg
-        
-        player = await fetch_player(pid)
-        teams = await fetch_team(player["tid"])
-        stats = await fetch_stats(pid)
-        ratings = await fetch_ratings(pid)
+            arg = int(arg)
+            player = list(filter(lambda player: player['pid'] == arg, main.export['players']))[0]
+                
+        else:
+            player = list(filter(lambda player: find_player_name(player) == arg, main.export['players']))
+            if len(player) > 1:
+                duplicates = True
+                if duplicates is True:
+
+                    def check(msg):
+                        return msg.author == ctx.author and msg.channel == ctx.channel  
+
+                    choices = "Multiple players found.\n"
+                    indexed_players = list(enumerate(player, start=1))
+
+                    for i_player in indexed_players:
+                        choices += f"Type {i_player[0]} for {i_player[1]['firstName']} (born {i_player[1]['born']['year']})\n"
+                    await ctx.send(choices)
+
+                    msg = await self.bot.wait_for('message', check=check)
+
+                    for index, i_player in indexed_players:
+                        if int(msg.content) == index:
+                            player = i_player
+
+        stats = player['stats'][-1]
+        ratings = player['ratings'][-1]    
+        team = list(filter(lambda team: team['tid'] or stats['tid'] == player['tid'], main.export['teams']))[0]
 
         physical_ratings = (
             f"**Hgt:** {ratings['hgt']}",
@@ -56,19 +90,13 @@ class Test(commands.Cog):
 
         )
 
-        if teams is None:
-            former_team = await fetch_team(stats["tid"])
-            player_info = f'[RETIRED] {find_player(player)}'
-            team_info = f'Last Team: {former_team["region"]} {former_team["name"]}'
-
-        else:
-            player_info = f'{stats["jerseyNumber"]} | {find_player(player)}'
-            team_info = f'{teams["region"]} {teams["name"]}'
+        player_info = f'{stats["jerseyNumber"]} | {find_player_name(player)}'
+        team_info = f'{team["region"]} {team["name"]}'
 
         embed = discord.Embed(title=player_info, description=f"**{ratings['pos']}** | {team_info}")
 
-        if teams is not None:
-            embed.set_thumbnail(url=teams['imgURL'])
+        if team is not None:
+            embed.set_thumbnail(url=team['imgURL'])
 
         embed.add_field(name="__Physical__", value="\n".join(physical_ratings))
         embed.add_field(name="__Shooting__", value="\n".join(shooting_ratings))
@@ -78,11 +106,14 @@ class Test(commands.Cog):
         await ctx.channel.send(embed=embed)
 
     # Search for team using region, name, or tid
-    # @commands.command(aliases=('t',))
-    # async def team(self, ctx, team: str):
-    #     data = dataa
-    #     embed = discord.Embed()
-    #     await ctx.channel.send(embed=embed)
+    # Make a command group for this?
+    # Roster (Season, Team Name, Team Rating, W-L) 
+    # SOS, Pyramid, Advanced Stats?
+    # Use hex info in export to determine embed color for each team
+    @commands.command(aliases=('t',))
+    async def team(self, ctx, tid: int, year: int = main.get_season()):
+        pass
+        # Not sure if I need an embed for this (yet)
 
 def setup(bot):
     bot.add_cog(Test(bot))
--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
 
+# Load export, define basic export functions
+# Launcher.py?
 if LATEST_EXPORT.lower().startswith('http'):
     req = urllib.request.Request(LATEST_EXPORT)
 else:
@@ -16,6 +18,27 @@ pass
 with urllib.request.urlopen(req) as f:
     export = json.loads(f.read().decode('utf-8-sig'))
 
+    def get_season():
+        season = export['gameAttributes']['season']
+        return season
+
+    def get_current_phase():
+        phase = export['gameAttributes']['phase'] 
+        return phase
+    
+    def get_teams():
+        teams = export['teams']
+        return teams
+
+    def get_players():
+        players = export['players']
+        return players
+
+    def get_schedule():
+        schedule = export['schedule']
+        return schedule
+
+# Load cogs
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py") and filename != "__init__.py":
         bot.load_extension(f'cogs.{filename[:-3]}')
